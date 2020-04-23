acafebcc211b619eb29e58383f968ffdfcf998d4
Ismael <ismael9996@gmail.com>
2020-04-23 13:26:19 +0100

    :red: taking template from resource

--- backend/src/main/java/com/huellapositiva/infrastructure/TemplateService.java
+++ backend/src/main/java/com/huellapositiva/infrastructure/TemplateService.java
@@ -3,13 +3,31 @@ package com.huellapositiva.infrastructure;
 import com.huellapositiva.domain.valueobjects.EmailTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 @Service
 public class TemplateService {
 
 
     public EmailTemplate getEmailConfirmationTemplate() {
-        return EmailTemplate.createEmailTemplate("Te acabas de registrar en huellapositiva.com\n" +
-                "    Para confirmar tu correo electrónico, haz clic en el enlace\n" +
-                "    <a href=\"${URL}\">Clic aquí</a>");
+        String template = "";
+        char[] array = new char[100];
+        try{
+            FileReader input = new FileReader("/emailConfirmation.txt");
+            System.out.println(input.read(array));
+            /*BufferedReader in = new BufferedReader(new FileReader("emailConfirmation.txt"));
+            String linea = in.readLine();
+            while (linea != null){
+                template = template + linea + '\n';
+                linea = in.readLine();
+            }
+            in.close();*/
+        }
+        catch(IOException ex){
+            System.out.println(ex);
+        }
+        return (EmailTemplate.createEmailTemplate(template));
     }
 }

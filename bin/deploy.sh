55e6ea7cf90e08b8293f3b71ec5faa05ce1d147f
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-04-26 21:44:41 +0200

    New: Add deploy remote script

---
+++ bin/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+env=$1
+
+ssh -t -t master@${env}.huelladigital.ayudadigital.org << EOF
+set -eu
+set -o pipefail
+cd platform/huelladigital
+git --no-pager diff
+git stash save "Deploy $(date '+%Y-%M-%d %H:%M:%S')"
+git checkout .
+git fetch -pv
+git pull
+cd platform/products/backend
+exit
+EOF

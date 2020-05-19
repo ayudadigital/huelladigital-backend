cf870715db92bbfc19ce0006e1f7ec2c20819d54
raulpadilladelgado <rpd27@outlook.com>
2020-05-19 12:40:04 +0100

    test IssueService saves FailEmailConfirmation

--- backend/src/test/java/com/huellapositiva/integration/RegisterVolunteerActionShould.java
+++ backend/src/test/java/com/huellapositiva/integration/RegisterVolunteerActionShould.java
@@ -1,26 +1,33 @@
 package com.huellapositiva.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.huellapositiva.application.dto.RegisterVolunteerRequestDto;
-import com.huellapositiva.domain.Email;
 import com.huellapositiva.domain.actions.RegisterVolunteerAction;
-import com.huellapositiva.domain.exception.EmailException;
 import com.huellapositiva.infrastructure.EmailService;
 import com.huellapositiva.infrastructure.orm.repository.JpaFailEmailConfirmationRepository;
 import com.huellapositiva.util.TestData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @Import(TestData.class)
 class RegisterVolunteerActionShould {
+
+    private static final String baseUri = "/api/v1/volunteers";
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mvc;
 
     @Autowired
     private RegisterVolunteerAction registerVolunteerAction;
@@ -50,26 +57,5 @@ class RegisterVolunteerActionShould {
         //THEN
         verify(emailService, times(1)).sendEmail(any());
     }
-
-
-    @Test
-    void fail_on_registering_a_volunteer_should_save_a_email_and_stacktrace() {
-        //GIVEN
-        RegisterVolunteerRequestDto dto = new RegisterVolunteerRequestDto("foo@huellapositiva.com", "plain-password");
-        Email email = Email.builder()
-                .from("foo@huellapositiva.com")
-                .to("paotro@huellapositiva.com")
-                .subject("test")
-                .body("test-body")
-                .build();
-
-        //WHEN
-        doThrow(new EmailException()).when(registerVolunteerAction).execute(dto);
-
-        //THEN
-        assertThat(failEmailConfirmationRepository.findByEmail(dto.getEmail()), is(dto.getEmail()));
-    }
-
-
 }
 
--- backend/src/test/java/com/huellapositiva/unit/IssueServiceShould.java
+++ backend/src/test/java/com/huellapositiva/unit/IssueServiceShould.java
@@ -1,5 +1,6 @@
 package com.huellapositiva.unit;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.huellapositiva.application.dto.RegisterVolunteerRequestDto;
 import com.huellapositiva.domain.actions.RegisterVolunteerAction;
 import com.huellapositiva.domain.exception.EmailException;
@@ -7,14 +8,20 @@ import com.huellapositiva.infrastructure.orm.repository.JpaFailEmailConfirmation
 import com.huellapositiva.infrastructure.orm.service.IssueService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class IssueServiceShould {
 
     @SpyBean
@@ -26,6 +33,12 @@ class IssueServiceShould {
     @Autowired
     JpaFailEmailConfirmationRepository failEmailConfirmationRepository;
 
+    private static final String baseUri = "/api/v1/volunteers";
+
+    @Autowired
+    private MockMvc mvc;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void verify_save_a_email() {
@@ -38,6 +51,23 @@ class IssueServiceShould {
         } catch (EmailException ex) {
             issueService.registerVolunteerIssue(dto.getEmail(), ex);
         }
+
+        //THEN
+        assertThat(failEmailConfirmationRepository.findByEmail(dto.getEmail()).get().getEmailAddress(), is(dto.getEmail()));
+    }
+
+    @Test
+    void fail_on_registering_a_volunteer_should_save_a_email_and_stacktrace() throws Exception {
+        //GIVEN
+        RegisterVolunteerRequestDto dto = new RegisterVolunteerRequestDto("foo@huellapositiva.com", "plain-password");
+
+        //WHEN
+        doThrow(new EmailException()).when(registerVolunteerAction).execute(dto);
+        String body = objectMapper.writeValueAsString(dto);
+        mvc.perform(post(baseUri)
+                .content(body)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
 
         //THEN
         assertThat(failEmailConfirmationRepository.findByEmail(dto.getEmail()).get().getEmailAddress(), is(dto.getEmail()));

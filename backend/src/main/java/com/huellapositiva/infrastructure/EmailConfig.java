0a6b455d80e60dcd8dfbe4c8195568c29a21ebe2
Ibai <ibai.usubiaga@gmail.com>
2020-05-23 20:07:05 +0100

    Feature/localstack local env (#9)
    * Add localstack configuration for local development
    
    * Add AWS properties for local development
    
    * Bind EmailConfig to email feature flag
    
    * Fix EmailConfig conditional setup

--- backend/src/main/java/com/huellapositiva/infrastructure/EmailConfig.java
+++ backend/src/main/java/com/huellapositiva/infrastructure/EmailConfig.java
@@ -6,27 +6,33 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
+@ConditionalOnProperty(name = "huellapositiva.feature.email.enabled", havingValue = "true")
 public class EmailConfig {
 
     @Autowired
     private AwsSesProperties awsSesProperties;
 
+    @Value("${huellapositiva.feature.email.enabled}")
+    private Boolean servusw;
+
     @Bean
-    @Profile({"!localstack"})
-    public AmazonSimpleEmailService getAwsSesClient(){
+    @Profile({"dev", "prod"})
+    public AmazonSimpleEmailService getAwsSesClient() {
         return AmazonSimpleEmailServiceClientBuilder.standard()
                 .withRegion(awsSesProperties.getRegion())
                 .build();
     }
 
     @Bean
-    @Profile("localstack")
-    public AmazonSimpleEmailService getLocalstackAwsSesClient(){
+    @Profile("!dev & !prod")
+    public AmazonSimpleEmailService getLocalstackAwsSesClient() {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsSesProperties.getAccessKey(), awsSesProperties.getSecretKey());
         return AmazonSimpleEmailServiceClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsSesProperties.getEndpoint(), awsSesProperties.getRegion()))
--- backend/src/test/java/com/huellapositiva/integration/AwsEmailServiceShould.java
+++ backend/src/test/java/com/huellapositiva/integration/AwsEmailServiceShould.java
@@ -11,12 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 
-@ActiveProfiles("localstack")
 @ExtendWith(AwsEnvVariablesExtension.class)
 @ExtendWith(LocalstackDockerExtension.class)
 @LocalstackDockerProperties(services = { "ses" })

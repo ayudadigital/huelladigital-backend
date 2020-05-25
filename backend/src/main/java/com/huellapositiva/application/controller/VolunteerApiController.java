26227144057a485f9b4736a2aa771af7e26fded8
José Ángel <josnih@gmail.com>
2020-05-25 20:27:30 +0100

    Refactor AWS SES

--- backend/src/main/java/com/huellapositiva/application/controller/VolunteerApiController.java
+++ backend/src/main/java/com/huellapositiva/application/controller/VolunteerApiController.java
@@ -6,12 +6,14 @@ import com.huellapositiva.domain.actions.RegisterVolunteerAction;
 import com.huellapositiva.domain.exception.EmailException;
 import com.huellapositiva.infrastructure.orm.service.IssueService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/volunteers")
@@ -24,13 +26,14 @@ public class VolunteerApiController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void registerVolunteer(@Validated @RequestBody RegisterVolunteerRequestDto dto) {
-        try{
+        try {
             registerVolunteerAction.execute(dto);
-        }catch(PasswordNotAllowed pna){
-            throw new ResponseStatusException( HttpStatus.BAD_REQUEST , "Password doesn't meet minimum length",  pna);
-        }catch (EmailException ex){
-            issueService.registerVolunteerIssue(dto.getEmail(),ex);
-            throw new ResponseStatusException( HttpStatus.INTERNAL_SERVER_ERROR, "Failed to send email confirmation", ex);
+        } catch (PasswordNotAllowed pna) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password doesn't meet minimum length", pna);
+        } catch (EmailException ex) {
+            log.error("Failed to send email:", ex);
+            issueService.registerVolunteerIssue(dto.getEmail(), ex);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to send email confirmation", ex);
         }
     }
 }

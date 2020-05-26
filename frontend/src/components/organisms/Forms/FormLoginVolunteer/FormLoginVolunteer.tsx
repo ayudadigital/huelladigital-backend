42719784ee42465052280dda31608fc6a28c8fad
unknown <airanschez0@gmail.com>
2020-05-26 13:35:25 +0100

    Try to implement connection with backend using axios instead of fetch

--- frontend/src/components/organisms/Forms/FormLoginVolunteer/FormLoginVolunteer.tsx
+++ frontend/src/components/organisms/Forms/FormLoginVolunteer/FormLoginVolunteer.tsx
@@ -1,70 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../styles.scss';
-import { FieldForm } from '../../../molecules/FieldForm';
-import { SubmitButton } from '../../../atoms/SubmitButton';
+import axios from 'axios';
+
 
 export const FormLoginVolunteer: React.FC<{}> = () => {
-  const [email, setemail] = useState('');
-  const [password, setpassword] = useState('');
 
-  const getDataUser = (data: any) => {
-    const inputTypeIsEmail = data[0] === 'email';
-    const inputTypeIsPassword = data[0] === 'password';
-    const valueInput = data[1];
-    if (inputTypeIsEmail) setemail(valueInput);
-    if (inputTypeIsPassword) setpassword(valueInput);
+  const getFormData = (event: any) => {
+    event.preventDefault();
   };
 
-  const URL = 'http://localhost:8080/api/v1/volunteers';
-  //const URL = 'https://webhook.site/63587131-e3bb-487e-857c-6fac629f0894';
-  /*
-  useEffect(() => {
-    fetch(URL, {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email: email, password: password }),
-    })
-      .then((response) => response.json())
-      .then((response) => console.log(response));
-  }, []); */
+  const submitData = (event: any) => {
+    event.preventDefault();
 
-  const handleSubmit = () => {
-    fetch(URL, {
-      method: 'POST',
-      body: JSON.parse(`{ "email": ${email} }`),
+    const data = {
+      email: "airandkjhasdk@gmail.com",
+      password: "asdasdasd"
+    }
+    /*
+    fetch('http://localhost:8080/api/v1/volunteers', {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(data), // data can be `string` or {object}!
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        if (response.status === 'success') {
-          alert('Message Sent.');
-        } else if (response.status === 'fail') {
-          alert('Message failed to send.');
-        }
-      });
+      mode: "no-cors"
+    }).then(res => console.log(res))
+      .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', response));
   };
+*/
 
+
+    axios({
+      method: 'post',
+      url: 'http://localhost:8080/api/v1/volunteers',
+      headers: {
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': false
+      },
+      data: JSON.stringify(data)
+    })
+      .then(response => {
+        console.log("Hola")
+        console.log(response);
+      })
+      .catch(err => console.log(err));
+
+  }
   return (
-    <form className="ContainerForm" method="POST" id="form" onSubmit={handleSubmit}>
+    <form className="ContainerForm" method="POST" id="form" onSubmit={submitData}>
       <h1>Acceso de voluntario</h1>
-      <FieldForm title={'Email'} type={'email'} name={'email'} getData={getDataUser} />
-      <FieldForm
-        title={'Contraseña'}
-        type={'password'}
-        name={'password'}
-        getData={getDataUser}
-      />
+      <input type="text" name="Correo" onChange={getFormData} />
+      <input type="password" name="Password" onChange={getFormData} />
+      <button type="submit">Enviar</button>
 
-      <SubmitButton text={'Acceder'} />
-      <p>¿Ya tiene cuenta? Iniciar sesión</p>
-      <p>{email}</p>
-      <p>{password}</p>
+
     </form>
   );
 };

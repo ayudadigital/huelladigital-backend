edae57ff701ce0ea131426cf1d86aee3dc191bf4
David <ddiaalv@gmail.com>
2020-05-27 10:36:03 +0100

    Switch logic from FormLoginVolunteer to FormRegisterVolunteer and uninstall axios

--- frontend/src/components/organisms/Forms/FormLoginVolunteer/FormLoginVolunteer.tsx
+++ frontend/src/components/organisms/Forms/FormLoginVolunteer/FormLoginVolunteer.tsx
@@ -1,60 +1,16 @@
 import React from 'react';
 import '../styles.scss';
-import axios from 'axios';
-
+import { FieldForm } from '../../../molecules/FieldForm';
+import { SubmitButton } from '../../../atoms/SubmitButton';
 
 export const FormLoginVolunteer: React.FC<{}> = () => {
-
-  const getFormData = (event: any) => {
-    event.preventDefault();
-  };
-
-  const submitData = (event: any) => {
-    event.preventDefault();
-
-    const data = {
-      email: "airandkjhasdk@gmail.com",
-      password: "asdasdasd"
-    }
-    /*
-    fetch('http://localhost:8080/api/v1/volunteers', {
-      method: 'POST', // or 'PUT'
-      body: JSON.stringify(data), // data can be `string` or {object}!
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      mode: "no-cors"
-    }).then(res => console.log(res))
-      .catch(error => console.error('Error:', error))
-      .then(response => console.log('Success:', response));
-  };
-*/
-
-
-    axios({
-      method: 'post',
-      url: 'http://localhost:8080/api/v1/volunteers',
-      headers: {
-        "Content-Type": "application/json",
-        'Access-Control-Allow-Origin': false
-      },
-      data: JSON.stringify(data)
-    })
-      .then(response => {
-        console.log("Hola")
-        console.log(response);
-      })
-      .catch(err => console.log(err));
-
-  }
   return (
-    <form className="ContainerForm" method="POST" id="form" onSubmit={submitData}>
+    <form className="ContainerForm" method="POST" id="form">
       <h1>Acceso de voluntario</h1>
-      <input type="text" name="Correo" onChange={getFormData} />
-      <input type="password" name="Password" onChange={getFormData} />
-      <button type="submit">Enviar</button>
-
-
+      <FieldForm title={'Email'} type={'email'} name={'email'} />
+      <FieldForm title={'Contraseña'} type={'password'} name={'password'} />
+      <SubmitButton text={'Acceder'} />
+      <p>¿Ya tiene cuenta? Iniciar sesión</p>
     </form>
   );
 };
--- frontend/src/components/organisms/Forms/FormRegisterVolunteer/FormRegisterVolunteer.tsx
+++ frontend/src/components/organisms/Forms/FormRegisterVolunteer/FormRegisterVolunteer.tsx
@@ -1,21 +1,63 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import '../styles.scss';
 import { FieldForm } from '../../../molecules/FieldForm';
 import { SubmitButton } from '../../../atoms/SubmitButton';
 
-export const FormRegisterVolunteer: React.FC<{}> = () => (
-  <form className="ContainerForm">
-    <h1>Registro de voluntario</h1>
-    <FieldForm title={'Email'} type={'email'} name={'email'} />
-    <FieldForm title={'Contraseña'} type={'password'} name={'password'} />
-    <FieldForm title={'Repetir contraseña'} type={'password'} name={'repeatPassword'} />
-    <label htmlFor="privacity">
-      <input type="checkbox" name="privacity" />
-      Acepto la política de privacidad del sitio.
-    </label>
-    <SubmitButton text={'Acceder'} />
-    <p>¿Ya tiene cuenta? Iniciar sesión</p>
-  </form>
-);
+export const FormRegisterVolunteer: React.FC<{}> = () => {
+  const [email, setemail] = useState('');
+  const [password, setpassword] = useState('');
+  const [repeatedPassword, setrepeatedPassword] = useState('');
+  const getDataUser = (data: any) => {
+    const inputTypeIsEmail = data[0] === 'email';
+    const inputTypeIsPassword = data[0] === 'password';
+    const inputTypeIsRepeatedPassword = data[0] === 'repeatedPassword';
+    const valueInput = data[1];
+    if (inputTypeIsEmail) setemail(valueInput);
+    if (inputTypeIsPassword) setpassword(valueInput);
+    if (inputTypeIsRepeatedPassword) setrepeatedPassword(valueInput);
+  };
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const URL = 'http://localhost:8080/api/v1/volunteers';
+    const data = {
+      email: email,
+      password: password,
+    };
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => console.log(res))
+      .catch((error) => console.error('Error:', error))
+      .then((response) => console.log('Success:', response));
+  };
+  return (
+    <form className="ContainerForm" method="POST" onSubmit={handleSubmit}>
+      <h1>Registro de voluntario</h1>
+      <FieldForm title={'Email'} type={'email'} name={'email'} getData={getDataUser} />
+      <FieldForm
+        title={'Contraseña'}
+        type={'password'}
+        name={'password'}
+        getData={getDataUser}
+      />
+      <FieldForm
+        title={'Repetir contraseña'}
+        type={'password'}
+        name={'repeatedPassword'}
+        getData={getDataUser}
+      />
+      <label htmlFor="privacity">
+        <input type="checkbox" name="privacity" />
+        Acepto la política de privacidad del sitio.
+      </label>
+      <SubmitButton text={'Acceder'} />
+      <p>¿Ya tiene cuenta? Iniciar sesión</p>
+    </form>
+  );
+};
 
 FormRegisterVolunteer.displayName = 'FormRegisterVolunteer';

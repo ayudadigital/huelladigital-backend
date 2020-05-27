6f4f15d0ef0b245bd4dcd6c0440008fbb9d6aca5
unknown <airanschez0@gmail.com>
2020-05-27 11:18:34 +0100

    Developing check password funtionality

--- frontend/src/components/organisms/Forms/FormRegisterVolunteer/FormRegisterVolunteer.tsx
+++ frontend/src/components/organisms/Forms/FormRegisterVolunteer/FormRegisterVolunteer.tsx
@@ -4,9 +4,12 @@ import { FieldForm } from '../../../molecules/FieldForm';
 import { SubmitButton } from '../../../atoms/SubmitButton';
 
 export const FormRegisterVolunteer: React.FC<{}> = () => {
+
   const [email, setemail] = useState('');
   const [password, setpassword] = useState('');
   const [repeatedPassword, setrepeatedPassword] = useState('');
+
+  // Obtener datos de los inputs
   const getDataUser = (data: any) => {
     const inputTypeIsEmail = data[0] === 'email';
     const inputTypeIsPassword = data[0] === 'password';
@@ -15,7 +18,38 @@ export const FormRegisterVolunteer: React.FC<{}> = () => {
     if (inputTypeIsEmail) setemail(valueInput);
     if (inputTypeIsPassword) setpassword(valueInput);
     if (inputTypeIsRepeatedPassword) setrepeatedPassword(valueInput);
+
+    // Llamada a checkPassWord
+    checkPassword(password, repeatedPassword)
   };
+
+  // Comprobar contraseñas 
+  const checkPassword = (pass: String, passrepeated: String) => {
+    // Requisitos contraseñas
+    console.log("Hola")
+    // 1- Mayor de 6 y menor de 20
+    if (checkLength(pass)) {
+      console.log("añadir color verde")
+    }
+    else {
+      console.log("añadir color rojo")
+    }
+    // 2- Mínimo 1 mayúscula y 1 minúscula
+    // 3- Mínimo 1 caracter raro (~! @ # $% ^& * -+ = ' | \ \ (){}\ []:; "' <>,.? /)
+    // 4- Mínimo 1 número
+
+  }
+
+  const checkLength = (password: String) => {
+    const minLenght: number = 5
+    const maxLenght: number = 19
+    let comprobar: boolean = false
+    if (password.length >= minLenght && password.length <= maxLenght)
+      comprobar = true;
+    return comprobar
+  }
+
+  // Conexión back-front
   const handleSubmit = (event: any) => {
     event.preventDefault();
     const URL = 'http://localhost:8080/api/v1/volunteers';
@@ -49,6 +83,7 @@ export const FormRegisterVolunteer: React.FC<{}> = () => {
         type={'password'}
         name={'repeatedPassword'}
         getData={getDataUser}
+
       />
       <label htmlFor="privacity">
         <input type="checkbox" name="privacity" />
@@ -56,6 +91,9 @@ export const FormRegisterVolunteer: React.FC<{}> = () => {
       </label>
       <SubmitButton text={'Acceder'} />
       <p>¿Ya tiene cuenta? Iniciar sesión</p>
+
+      <p>{password}</p>
+      <p>{repeatedPassword}</p>
     </form>
   );
 };

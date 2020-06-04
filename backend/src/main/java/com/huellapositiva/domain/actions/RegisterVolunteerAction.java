6a06919c9cc16a9d57461e4c93d06a5cd423f353
josnih21 <48065983+josnih21@users.noreply.github.com>
2020-06-04 14:16:43 +0100

    Refactor EmailCommunicationService (#15)

--- backend/src/main/java/com/huellapositiva/domain/actions/RegisterVolunteerAction.java
+++ backend/src/main/java/com/huellapositiva/domain/actions/RegisterVolunteerAction.java
@@ -5,38 +5,28 @@ import com.huellapositiva.domain.service.EmailCommunicationService;
 import com.huellapositiva.domain.service.VolunteerService;
 import com.huellapositiva.domain.valueobjects.EmailConfirmation;
 import com.huellapositiva.domain.valueobjects.PlainPassword;
-import com.huellapositiva.infrastructure.EmailService;
-import com.huellapositiva.infrastructure.TemplateService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterVolunteerAction {
+
     private final VolunteerService volunteerService;
-    private EmailService emailService;
-    private TemplateService templateService;
-    private EmailCommunicationService communicationService;
+
+    private final EmailCommunicationService communicationService;
 
     @Value("${huellapositiva.api.v1.confirmation-email}")
     private String emailConfirmationBaseUrl;
 
-    public RegisterVolunteerAction(VolunteerService volunteerService, EmailService emailService, TemplateService templateService) {
+    public RegisterVolunteerAction(VolunteerService volunteerService, EmailCommunicationService communicationService) {
         this.volunteerService = volunteerService;
-        this.emailService = emailService;
-        this.templateService = templateService;
-    }
-
-    public RegisterVolunteerAction(VolunteerService volunteerService, EmailCommunicationService emailCommunicationService) {
-        this.volunteerService = volunteerService;
-        this.communicationService = emailCommunicationService;
+        this.communicationService = communicationService;
     }
 
     public void execute(RegisterVolunteerRequestDto dto) {
         EmailConfirmation emailConfirmation = EmailConfirmation.from(dto.getEmail(), emailConfirmationBaseUrl);
         volunteerService.registerVolunteer(PlainPassword.from(dto.getPassword()), emailConfirmation);
-        new EmailCommunicationService(emailService, templateService)
-                .sendRegistrationConfirmationEmail(emailConfirmation);
-        // TODO: communicationService.sendRegistrationConfirmationEmail(emailConfirmation);
+        communicationService.sendRegistrationConfirmationEmail(emailConfirmation);
     }
 
 }

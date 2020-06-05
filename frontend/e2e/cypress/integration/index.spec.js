1bae161849ecbb6f31381b6b4b15a13944183e0d
Rubén Zagon <ruben.zagon@gmail.com>
2020-06-05 08:13:30 +0100

    Add cypress test e2e for Volunteer behavior

--- frontend/e2e/cypress/integration/index.spec.js
+++ frontend/e2e/cypress/integration/index.spec.js
@@ -1,5 +1,19 @@
-describe('', () => {
-  it('should open the web', () => {
-    expect(true).to.equal(true);
+describe('Volunteer should', () => {
+  it('open the web', () => {
+    cy.visit('/');
   });
+
+  it('visit the registration page', () => {
+    cy.visit('/volunteer-register');
+    cy.get('form');
+    cy.get('h1').should('contain', 'Registro');
+  });
+
+  it('visit the login page', () => {
+    cy.visit('/volunteer-login');
+    cy.get('form');
+    cy.get('h1').should('contain', 'Acceso');
+  });
+
+
 });

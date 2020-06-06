8759dfd275bc0eda559c23654f19114264f4389b
Rubén Zagon <ruben.zagon@gmail.com>
2020-06-06 01:23:06 +0100

    Add more and specifics test FormRegisterVolunteer

--- frontend/src/components/organisms/Forms/FormRegisterVolunteer/FormRegisterVolunteer.spec.tsx
+++ frontend/src/components/organisms/Forms/FormRegisterVolunteer/FormRegisterVolunteer.spec.tsx
@@ -10,11 +10,11 @@ const EMAIL = {
 
 const PASSWORD = {
   allowed: 'aLongPassword',
-  wrong: 'short'
+  wrong: 'short',
 };
 
-describe('FormRegisterVolunteer', () => {
-  it('should disabled the button submit when have any part of the form is wrong', () => {
+describe('FormRegisterVolunteer should', () => {
+  it('disabled the button submit when have any part of the form is wrong', () => {
     const renderResult: RenderResult = render(
       <BrowserRouter>
         <FormRegisterVolunteer/>,
@@ -24,7 +24,47 @@ describe('FormRegisterVolunteer', () => {
     expect(submitButton.disabled).toBe(true);
   });
 
-  it('should enable the button submit when all fields are fill with allowed values', () => {
+  it('display a message when email are not allowed or is wrong', () => {
+    const renderResult: RenderResult = render(
+      <BrowserRouter>
+        <FormRegisterVolunteer/>,
+      </BrowserRouter>,
+    );
+
+    const emailField = renderResult.queryAllByLabelText('input-form')[0];
+    fireEvent.change(emailField, { target: { value: EMAIL.wrong } });
+    expect(renderResult.queryByText('El email introducido no es válido')).toBeTruthy();
+  });
+
+  it('display a message when password is too short', () => {
+    const renderResult: RenderResult = render(
+      <BrowserRouter>
+        <FormRegisterVolunteer/>,
+      </BrowserRouter>,
+    );
+
+    const passwordField = renderResult.queryAllByLabelText('input-form')[1];
+    fireEvent.change(passwordField, { target: { value: PASSWORD.wrong } });
+    expect(renderResult.queryByText('Contraseña demasiado corta', {exact:false})).toBeTruthy();
+  });
+
+  it('display a message when repeat password is not the same', () => {
+    const renderResult: RenderResult = render(
+      <BrowserRouter>
+        <FormRegisterVolunteer/>,
+      </BrowserRouter>,
+    );
+
+    const passwordField = renderResult.queryAllByLabelText('input-form')[1];
+    fireEvent.change(passwordField, { target: { value: PASSWORD.allowed } });
+
+    const repeatPasswordField = renderResult.queryAllByLabelText('input-form')[2];
+    fireEvent.change(repeatPasswordField, { target: { value: PASSWORD.wrong } });
+
+    expect(renderResult.queryByText('Las contraseñas no coinciden')).toBeTruthy();
+  });
+
+  it('enable the button submit when all fields are fill with allowed values', () => {
     const renderResult: RenderResult = render(
       <BrowserRouter>
         <FormRegisterVolunteer/>,
@@ -33,38 +73,17 @@ describe('FormRegisterVolunteer', () => {
     const submitButton = renderResult.queryByLabelText('submit-button');
 
     const emailField = renderResult.queryAllByLabelText('input-form')[0];
-    expect(emailField.name).toContain('email');
-    expect(emailField.value).toContain('');
-    fireEvent.change(emailField, {
-      target:
-        {
-          value: EMAIL.allowed,
-        },
-    });
+    fireEvent.change(emailField, { target: { value: EMAIL.allowed } });
     expect(emailField.value).toContain(EMAIL.allowed);
     expect(submitButton.disabled).toBe(true);
 
     const passwordField = renderResult.queryAllByLabelText('input-form')[1];
-    expect(passwordField.name).toContain('password');
-    expect(passwordField.value).toContain('');
-    fireEvent.change(passwordField, {
-      target:
-        {
-          value: PASSWORD.allowed,
-        },
-    });
+    fireEvent.change(passwordField, { target: { value: PASSWORD.allowed } });
     expect(passwordField.value).toContain(PASSWORD.allowed);
     expect(submitButton.disabled).toBe(true);
 
     const repeatPasswordField = renderResult.queryAllByLabelText('input-form')[2];
-    expect(repeatPasswordField.name).toContain('repeatedPassword');
-    expect(repeatPasswordField.value).toContain('');
-    fireEvent.change(repeatPasswordField, {
-      target:
-        {
-          value: PASSWORD.allowed,
-        },
-    });
+    fireEvent.change(repeatPasswordField, { target: { value: PASSWORD.allowed } });
     expect(repeatPasswordField.value).toContain(PASSWORD.allowed);
     expect(submitButton.disabled).toBe(false);
   });

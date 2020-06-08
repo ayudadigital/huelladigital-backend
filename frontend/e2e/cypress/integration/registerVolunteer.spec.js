12321a7a07554eb065e3e13c2b0ce30a0bcb014a
Rubén Zagon <ruben.zagon@gmail.com>
2020-06-08 11:45:45 +0100

    Refactor test e2e and add test to verify behavior when email is registered

--- frontend/e2e/cypress/integration/registerVolunteer.spec.js
+++ frontend/e2e/cypress/integration/registerVolunteer.spec.js
@@ -1,22 +1,51 @@
+import { ROUTE } from '../../../src/utils/routes';
+
 describe('Volunteer should', () => {
 
-  it('open the web', () => {
+  const VOLUNTEER = {
+    email: {
+      random: `test${(Math.random() * 1000).toString()}@test.com`,
+      duplicate: `email.is.registered@test.com`
+    },
+
+    password: 'aLongPassword'
+  };
+
+  beforeEach(() => {
     cy.visit('/');
+    cy.get('button[aria-label=register-button]').click();
+    cy.get('input[name=email]').type(`${VOLUNTEER.email.duplicate}`);
+    cy.get('input[name=password]').type(`${VOLUNTEER.password}`);
+    cy.get('input[name=repeatedPassword]').type(`${VOLUNTEER.password}`);
+    cy.get('button[type=submit]').click();
+  });
+
+  it('open the web', () => {
+    cy.visit(ROUTE.home);
   });
 
   it('verify resend to email confirmation page when the form is good way', () => {
-    const randomEmail = (Math.random() * 1000).toString();
-    const {email, password} = {email: `test${randomEmail}@test.com`, password: 'aLongPassword'};
-
-    cy.visit('/');
+    cy.visit(ROUTE.home);
     cy.get('button[aria-label=register-button]').click();
-    cy.get('input[name=email]').type(`${email}`);
-    cy.get('input[name=password]').type(`${password}`);
-    cy.get('input[name=repeatedPassword]').type(`${password}`);
+    cy.get('input[name=email]').type(`${VOLUNTEER.email.random}`);
+    cy.get('input[name=password]').type(`${VOLUNTEER.password}`);
+    cy.get('input[name=repeatedPassword]').type(`${VOLUNTEER.password}`);
     cy.get('button[type=submit]').click();
 
     cy.url().should('include', '/email-confirmation-register');
-    cy.get('p').should('contain', 'reenviar correo')
+    cy.get('p').should('contain', 'reenviar correo');
+  });
+
+  xit('tell user that email are register', () => {
+    cy.visit(ROUTE.home);
+    cy.get('button[aria-label=register-button]').click();
+    cy.get('input[name=email]').type(`${VOLUNTEER.email.duplicate}`);
+    cy.get('input[name=password]').type(`${VOLUNTEER.password}`);
+    cy.get('input[name=repeatedPassword]').type(`${VOLUNTEER.password}`);
+    cy.get('button[type=submit]').click();
+
+    cy.url().should('include', '/');
+    cy.get('p').should('contain', 'Este email ya existe');
   });
 
 

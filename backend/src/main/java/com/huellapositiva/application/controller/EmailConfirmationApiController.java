d01501eb65df0f19b4b888e946255613616db2ba
juanma <jmlpdr.93@gmail.com>
2020-06-08 13:25:06 +0100

    Tested resend email confirmation action

--- backend/src/main/java/com/huellapositiva/application/controller/EmailConfirmationApiController.java
+++ backend/src/main/java/com/huellapositiva/application/controller/EmailConfirmationApiController.java
@@ -1,8 +1,10 @@
 package com.huellapositiva.application.controller;
 
 import com.huellapositiva.domain.actions.EmailConfirmationAction;
+import com.huellapositiva.domain.actions.ResendEmailConfirmationAction;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,14 +15,22 @@ import java.util.UUID;
 @RequestMapping("/api/v1/email-confirmation")
 public class EmailConfirmationApiController {
 
-
     @Autowired
     private EmailConfirmationAction emailConfirmationAction;
 
+    @Autowired
+    private ResendEmailConfirmationAction resendEmailConfirmationAction;
+
     @GetMapping("/{hash}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void confirmEmail(@PathVariable UUID hash){
+    public void confirmEmail(@PathVariable UUID hash) {
         emailConfirmationAction.execute(hash);
     }
 
+    @GetMapping("/resend")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void resendConfirmEmail(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+
+        resendEmailConfirmationAction.execute(token);
+    }
 }

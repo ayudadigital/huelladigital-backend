157902adbfddcd0aeae8fe0061e42dac57896244
Rubén Zagon <ruben.zagon@gmail.com>
2020-06-08 13:01:58 +0100

    Refactor img atom component

--- frontend/src/components/atoms/Image/Image.stories.tsx
+++ frontend/src/components/atoms/Image/Image.stories.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import {Image} from './Image';
+import { Image } from './Image';
 import { withA11y } from '@storybook/addon-a11y';
 import logo from '../../../../public/logo192.png';
+import superHeroes from './assets/superHeroes.svg';
+import { Fragment } from 'react';
+import { text, withKnobs } from '@storybook/addon-knobs';
+
 export default {
   title: 'Atom | Image',
-  decorators: [withA11y],
+  decorators: [withA11y, withKnobs],
 };
 
-export const withText = () => <Image source={logo} description={"hola"}/>;
+export const Logo = () => <Image source={logo} description={'logo'} width={text('Ancho', '100px')}/>;
+export const SuperHeroes = () => {
+  return (
+    <Fragment>
+      <Image source={superHeroes} description={'super heroes logo register'}/>
+      <Image source={superHeroes} description={'super heroes logo register'} width={'300px'}/>
+      <Image source={superHeroes} description={'super heroes logo register'} width={'10%'}/>
+    </Fragment>
+
+  );
+};
--- frontend/src/components/atoms/Image/Image.tsx
+++ frontend/src/components/atoms/Image/Image.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 import './Image.scss';
 
 interface ImageProps {
-  source:string,
-  description: string
-} 
+  source:string;
+  description: string;
+  width?: string;
+}
 
-export const Image: React.FC<ImageProps> = ({source, description}) => (
-  <img src={source} alt={description}/>
+export const Image: React.FC<ImageProps> = ({source, description= 'image', width}) => (
+  <img src={source} alt={description} width={width}/>
 );
 
 Image.displayName = 'Image';

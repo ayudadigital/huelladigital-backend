cfdc80d4ec25b14c47979fdcbf058e2c71c58cd8
David <ddiaalv@gmail.com>
2020-06-09 10:41:00 +0100

    Added new Home style and a svg image for register volunteer.

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import './styles.scss';
 import { WrapperPages } from '../../components/templates/WrapperPages';
 import { MixForms } from '../../components/organisms/Forms/MixForms';
+import register from './assets/register.svg';
 
+const bgStyle = {
+  background: `url(${register}) no-repeat top center`,
+  backgroundSize: '100%',
+};
 
 export const Home: React.FC<{}> = () => {
   return (
     <WrapperPages>
-      <header/>
-      <section className={'Home'}>
-        <MixForms/>
+      <header />
+      <section className={'Home'} style={bgStyle}>
+        <MixForms />
       </section>
-      <footer/>
+      <footer />
     </WrapperPages>
   );
 };

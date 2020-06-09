19f25cd100e9b8901e0dbadf8f73a35614ffa7a0
David <ddiaalv@gmail.com>
2020-06-09 15:37:26 +0100

    Added styles to Home and Header

--- frontend/src/components/organisms/Header/Header.tsx
+++ frontend/src/components/organisms/Header/Header.tsx
@@ -2,11 +2,21 @@ import * as React from 'react';
 import './Header.scss';
 import { Image } from '../../atoms/Image';
 import { NavBar } from '../../molecules/NavBar';
+import waveHeader from './assets/wave-header.svg';
+
+const bgStyle = {
+  background: `url(${waveHeader}) no-repeat center bottom,linear-gradient(rgba(126, 37, 78, 0.45), rgba(126, 37, 78, 0.45)), url(https://huellapositiva.com/wp-content/uploads/2020/03/remi-walle-UOwvwZ9Dy6w-unsplash.jpg)
+    no-repeat center`,
+  backgroundSize: '110%, auto, cover',
+};
 
 export const Header: React.FC<{}> = () => (
-  <div className="Header">
-    <Image source="https://huellapositiva.com/wp-content/uploads/2020/03/cropped-Logo-Huella-Positiva-PV-05.png" description="logo"/>
-    <NavBar/>
+  <div className="Header" style={bgStyle}>
+    <Image
+      source="https://huellapositiva.com/wp-content/uploads/2020/03/cropped-Logo-Huella-Positiva-PV-05.png"
+      description="logo"
+    />
+    <NavBar />
   </div>
 );
 
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,19 +2,13 @@ import React from 'react';
 import './styles.scss';
 import { WrapperPages } from '../../components/templates/WrapperPages';
 import { MixForms } from '../../components/organisms/Forms/MixForms';
-import register from './assets/register.svg';
 import { Footer } from '../../components/organisms/Footer';
-
-const bgStyle = {
-  background: `url(${register}) no-repeat top center`,
-  backgroundSize: '100%',
-};
 
 export const Home: React.FC<{}> = () => {
   return (
     <WrapperPages>
       <header />
-      <section className={'Home'} style={bgStyle}>
+      <section className={'Home'}>
         <MixForms />
       </section>
       <Footer />

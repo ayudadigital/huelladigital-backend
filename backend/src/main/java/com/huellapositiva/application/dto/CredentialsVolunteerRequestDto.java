526c06b248b90d63bfb93c928ef1bc0e3c1024af
Rubén Zagon <ruben.zagon@gmail.com>
2020-07-03 13:13:20 +0100

    Documented volunteer API

--- backend/src/main/java/com/huellapositiva/application/dto/CredentialsVolunteerRequestDto.java
+++ backend/src/main/java/com/huellapositiva/application/dto/CredentialsVolunteerRequestDto.java
@@ -1,6 +1,9 @@
 package com.huellapositiva.application.dto;
 
-import lombok.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
@@ -11,10 +14,19 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 public class CredentialsVolunteerRequestDto {
 
+
+    @Schema(
+            description = "Volunteer email unique",
+            example = "jhon.doe@gmail.com"
+    )
     @NotNull
     @Email(regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
     private final String email;
 
+    @Schema(
+            description = "Volunteer password",
+            example = "albaricoque28"
+    )
     @NotEmpty
     private final String password;
 }

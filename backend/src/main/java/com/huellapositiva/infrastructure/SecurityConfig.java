7e2cfc05b368dbaa6370a0ae21a71164942d6412
Rubén Zagon <ruben.zagon@gmail.com>
2020-07-03 11:39:44 +0100

    Update to OpenAPI3

--- backend/src/main/java/com/huellapositiva/infrastructure/SecurityConfig.java
+++ backend/src/main/java/com/huellapositiva/infrastructure/SecurityConfig.java
@@ -72,14 +72,14 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//            http.csrf().disable()
-//                    .authorizeRequests()
-//                    .antMatchers("/**").permitAll()
-//                    .anyRequest().permitAll();
-
-            http.authorizeRequests()
-                .antMatchers(AUTH_ALLOWSLIST).permitAll()
-                    .anyRequest().authenticated();
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/**").permitAll()
+                    .anyRequest().permitAll();
+//
+//            http.authorizeRequests()
+//                .antMatchers(AUTH_ALLOWSLIST).permitAll()
+//                    .anyRequest().authenticated();
 
 
 
--- backend/src/main/java/com/huellapositiva/infrastructure/documentation/SwaggerConfig.java
+++ backend/src/main/java/com/huellapositiva/infrastructure/documentation/SwaggerConfig.java
@@ -1,42 +1,42 @@
-package com.huellapositiva.infrastructure.documentation;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@Configuration
-@EnableSwagger2
-@Profile("dev")
-@Import(BeanValidatorPluginsConfiguration.class)
-public class SwaggerConfig {
-
-    @Value("${info.build.version}")
-    private String buildVersion;
-
-    @Bean
-    public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build().apiInfo(apiInfo());
-
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("Huella Positiva")
-                .description("Plataforma de voluntariado en Canarias")
-                .version(buildVersion)
-                .build();
-    }
-}
+//package com.huellapositiva.infrastructure.documentation;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Import;
+//import org.springframework.context.annotation.Profile;
+//import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@Configuration
+//@EnableSwagger2
+//@Profile("dev")
+//@Import(BeanValidatorPluginsConfiguration.class)
+//public class SwaggerConfig {
+//
+//    @Value("${info.build.version}")
+//    private String buildVersion;
+//
+//    @Bean
+//    public Docket apiDocket() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build().apiInfo(apiInfo());
+//
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder().title("Huella Positiva")
+//                .description("Plataforma de voluntariado en Canarias")
+//                .version(buildVersion)
+//                .build();
+//    }
+//}

3f192d7e2d00e38ffe3e350842eb3a2852408998
Rubén Zagon <ruben.zagon@gmail.com>
2020-07-06 15:06:03 +0100

    Add the servers on info

--- backend/src/main/java/com/huellapositiva/infrastructure/documentation/OpenAPIConfig.java
+++ backend/src/main/java/com/huellapositiva/infrastructure/documentation/OpenAPIConfig.java
@@ -6,9 +6,12 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -28,13 +31,20 @@ public class OpenAPIConfig {
     @Value("${info.build.version}")
     private String buildVersion;
 
+    private final List<Server> servers = List.of(
+            new Server().url("http://localhost:8080/").description("Generated server url"),
+            new Server().url("https://dev.huelladigital.ayudadigital.org/").description("Environment of pre-production"),
+            new Server().url("https://plataforma.huelladigital.ayudadigital.org/").description("Production environment")
+            );
+
     @Bean
     public OpenAPI customConfiguration() {
         return new OpenAPI()
                 .components(new Components())
                 .info(new Info().title("Huella Positiva")
                         .description("Plataforma de voluntariado en Canarias")
-                        .version(buildVersion));
+                        .version(buildVersion))
+                .servers(servers);
 
     }
 }

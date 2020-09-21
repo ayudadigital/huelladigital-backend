968f468894d157abc3299328095c0f7beb43482d
Ibai Usubiaga <ibai.usubiaga@gmail.com>
2020-09-21 20:13:18 +0200

    Consider empty extensions when uploading images

--- backend/src/main/java/com/huellapositiva/domain/service/RemoteStorageService.java
+++ backend/src/main/java/com/huellapositiva/domain/service/RemoteStorageService.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,13 +20,14 @@ public class RemoteStorageService {
     private final StorageService storageService;
 
     public URL uploadProposalImage(MultipartFile file, String proposalId) throws IOException {
-        String destinationFileName = UUID.randomUUID() + getExtension(file.getOriginalFilename());
+        String basename = UUID.randomUUID().toString();
+        String destinationFileName = getExtension(file.getOriginalFilename()).map(basename::concat).orElse(basename);
         String proposalImageRootKey = "images/proposals/" + proposalId + '/';
         return storageService.upload(proposalImageRootKey + destinationFileName, file.getInputStream(), file.getContentType());
     }
 
     public URL uploadVolunteerCV(MultipartFile file, String volunteerId) throws IOException {
-        String extension = getExtension(file.getOriginalFilename());
+        String extension = getExtension(file.getOriginalFilename()).orElse("");
         if(!".pdf".equalsIgnoreCase(extension)) {
             throw new FileTypeNotSupported("Curriculum vitae file must be .pdf");
         }
@@ -34,11 +36,10 @@ public class RemoteStorageService {
         return storageService.upload(volunteerCVRootKey + destinationFileName, file.getInputStream(), file.getContentType());
     }
 
-    private String getExtension(String fileName) {
-        if (fileName == null) {
-            return "";
-        }
-        int index = fileName.lastIndexOf('.');
-        return index != -1 ? fileName.substring(index) : "";
+    private Optional<String> getExtension(String fileName) {
+        return Optional.ofNullable(fileName).map(filename -> {
+                    int index = fileName.lastIndexOf('.');
+                    return index >= 0 ? fileName.substring(index) : null;
+                });
     }
 }

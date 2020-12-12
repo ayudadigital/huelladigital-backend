dee38f60d8194fcd6817d05ae154e09873654e8b
Ibai Usubiaga <ibai.usubiaga@gmail.com>
2020-12-12 11:20:58 +0000

    Set SSM client during bootstrap

--- backend/src/main/java/com/huellapositiva/infrastructure/AwsSsmConfig.java
+++ backend/src/main/java/com/huellapositiva/infrastructure/AwsSsmConfig.java
@@ -1,6 +1,7 @@
 package com.huellapositiva.infrastructure;
 
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
 import lombok.extern.slf4j.Slf4j;
@@ -10,19 +11,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 @Slf4j
-@Profile({"dev", "prod"})
+@Profile("test-ssm")
 @BootstrapConfiguration
-public class BootstrapConfig {
+public class AwsSsmConfig {
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    @Value("${aws.ssm.endpoint}")
+    private String ssmUrl;
+
     @Bean
+    @Profile({"dev", "prod"})
     public AWSSimpleSystemsManagement awsSimpleSystemsManagement() {
         log.info("Setting up parameter-store in {} region ...", region);
         return AWSSimpleSystemsManagementClientBuilder.standard()
                 .withCredentials(InstanceProfileCredentialsProvider.getInstance())
                 .withRegion(region)
+                .build();
+    }
+
+    @Bean
+    @Profile("test-ssm")
+    public AWSSimpleSystemsManagement localAwsSimpleSystemsManagement() {
+        log.info("Setting up local parameter-store {} in {} region ...", ssmUrl, region);
+        return AWSSimpleSystemsManagementClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4583", region))
                 .build();
     }
 }

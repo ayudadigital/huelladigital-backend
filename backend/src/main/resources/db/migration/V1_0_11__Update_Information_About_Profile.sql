aaf4f359edca02c931b48372399ff02fac0ca06e
Juanma <jmlpdr.93@gmail.com>
2020-12-28 11:03:32 +0000

    * UPDATE: Created profile table

--- backend/src/main/resources/db/migration/V1_0_11__Update_Information_About_Profile.sql
+++ backend/src/main/resources/db/migration/V1_0_11__Update_Information_About_Profile.sql
@@ -17,6 +17,24 @@ ALTER TABLE locations
 ALTER TABLE volunteers
     DROP COLUMN age;
 
+CREATE TABLE profile
+(
+    id                      VARCHAR(255) UNIQUE NOT NULL,
+    surrogate_key           SERIAL PRIMARY KEY,
+    name                    VARCHAR(255) NOT NULL,
+    surname                 VARCHAR(255) NOT NULL,
+    phone_number            VARCHAR(255) NOT NULL,
+    birth_date              DATE NOT NULL,
+    location_id             VARCHAR(255),
+    curriculum_vitae_url    VARCHAR(255),
+    photo_url               VARCHAR(255),
+    twitter                 VARCHAR(255),
+    instagram               VARCHAR(255),
+    linkedin                VARCHAR(255),
+    additional_information  VARCHAR(500),
+    FOREIGN KEY (location_id) REFERENCES locations(id)
+);
+
 ALTER TABLE volunteers
     ADD COLUMN twitter VARCHAR(255) NULL;
 ALTER TABLE volunteers
@@ -27,3 +45,9 @@ ALTER TABLE volunteers
     ADD COLUMN photo VARCHAR(255);
 ALTER TABLE volunteers
     ADD COLUMN additional_information VARCHAR(500);
+ALTER TABLE volunteers
+    ADD COLUMN profile_id VARCHAR(255);
+ALTER TABLE volunteers
+    ADD CONSTRAINT volunteers_profile_id_fkey
+    FOREIGN KEY (profile_id)
+    REFERENCES profile(id);

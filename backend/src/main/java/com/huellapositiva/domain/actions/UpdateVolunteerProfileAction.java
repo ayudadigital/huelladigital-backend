f551246481dda3a5bc7935423cf2e8c8750da7dc
JeremyTrujillo <jeremy.trujillo.jt15@gmail.com>
2021-01-11 11:25:06 +0000

    * ADD: Added role change when email is modified. * REFACTOR: Renamed method.

--- backend/src/main/java/com/huellapositiva/domain/actions/UpdateVolunteerProfileAction.java
+++ backend/src/main/java/com/huellapositiva/domain/actions/UpdateVolunteerProfileAction.java
@@ -3,16 +3,23 @@ package com.huellapositiva.domain.actions;
 import com.huellapositiva.application.dto.ProfileDto;
 import com.huellapositiva.application.exception.EmailAlreadyExistsException;
 import com.huellapositiva.application.exception.InvalidFieldException;
+import com.huellapositiva.domain.exception.RoleNotFoundException;
 import com.huellapositiva.domain.model.valueobjects.*;
 import com.huellapositiva.domain.service.EmailCommunicationService;
 import com.huellapositiva.infrastructure.orm.entities.JpaLocation;
 import com.huellapositiva.infrastructure.orm.entities.JpaProfile;
 import com.huellapositiva.infrastructure.orm.entities.JpaVolunteer;
+import com.huellapositiva.infrastructure.orm.entities.Role;
 import com.huellapositiva.infrastructure.orm.repository.JpaCredentialRepository;
+import com.huellapositiva.infrastructure.orm.repository.JpaRoleRepository;
 import com.huellapositiva.infrastructure.orm.repository.JpaVolunteerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import static com.huellapositiva.domain.model.valueobjects.Roles.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UpdateVolunteerProfileAction {
@@ -21,6 +28,9 @@ public class UpdateVolunteerProfileAction {
 
     @Autowired
     private JpaCredentialRepository jpaCredentialRepository;
+
+    @Autowired
+    private JpaRoleRepository jpaRoleRepository;
 
     @Autowired
     private EmailCommunicationService emailCommunicationService;
@@ -48,6 +58,12 @@ public class UpdateVolunteerProfileAction {
         jpaVolunteerRepository.save(jpaVolunteer);
 
         if (isNotEqualsEmail) {
+            Role newJpaRole = jpaRoleRepository.findByName(VOLUNTEER_NOT_CONFIRMED.toString())
+                    .orElseThrow(() -> new RoleNotFoundException("Role " + VOLUNTEER_NOT_CONFIRMED.toString() + "not found."));
+            Set<Role> newUserRoles = new HashSet<>();
+            newUserRoles.add(newJpaRole);
+            jpaVolunteer.getCredential().setRoles(newUserRoles);
+            jpaVolunteerRepository.save(jpaVolunteer);
             EmailConfirmation emailConfirmation = EmailConfirmation.from(profileDto.getEmail(), emailConfirmationBaseUrl);
             emailCommunicationService.sendMessageEmailChanged(emailConfirmation);
         }
@@ -72,7 +88,7 @@ public class UpdateVolunteerProfileAction {
         if (PhoneNumber.isNotPhoneNumber(profileDto.getPhoneNumber())) {
             throw new InvalidFieldException("The phone number field is invalid");
         }
-        if (AdditionalInformation.lengthValidation(profileDto.getAdditionalInformation())) {
+        if (AdditionalInformation.isLengthInvalid(profileDto.getAdditionalInformation())) {
             throw new InvalidFieldException("The additional information field is invalid");
         }
     }
--- backend/src/main/java/com/huellapositiva/domain/model/valueobjects/AdditionalInformation.java
+++ backend/src/main/java/com/huellapositiva/domain/model/valueobjects/AdditionalInformation.java
@@ -10,7 +10,7 @@ import lombok.Data;
 public class AdditionalInformation {
     private String information;
 
-    public static boolean lengthValidation(String additionalInformation) {
+    public static boolean isLengthInvalid(String additionalInformation) {
         return additionalInformation != null && additionalInformation.length() > 500;
     }
 }

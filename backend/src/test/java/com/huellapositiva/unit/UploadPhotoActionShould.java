8245adcbdcf9b3d53e1c700690d84e6b2bae567e
Juanma <jmlpdr.93@gmail.com>
2021-01-15 18:52:48 +0000

    * ADD: Added unit test in UploadPhotoActionShould

---
+++ backend/src/test/java/com/huellapositiva/unit/UploadPhotoActionShould.java
@@ -0,0 +1,50 @@
+package com.huellapositiva.unit;
+
+import com.huellapositiva.application.exception.InvalidFieldException;
+import com.huellapositiva.domain.actions.UploadPhotoAction;
+import com.huellapositiva.domain.model.valueobjects.PhoneNumber;
+import com.huellapositiva.util.TestData;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import static com.huellapositiva.util.TestData.DEFAULT_EMAIL;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestData.class)
+public class UploadPhotoActionShould {
+
+    @Autowired
+    private UploadPhotoAction uploadPhotoAction;
+
+    @ParameterizedTest
+    @MethodSource("provideIncorrectPhotos")
+    void return_exception_if_do_not_valid_image(String image) throws IOException {
+        String name = "photo.png";
+        String originalFileName = image;
+        String contentType = "image/png";
+        byte[] content = getClass().getClassLoader().getResourceAsStream("images/" + image).readAllBytes();
+
+        MultipartFile result = new MockMultipartFile(name, originalFileName, contentType, content);
+
+        assertThrows(InvalidFieldException.class, () -> {
+            uploadPhotoAction.execute(result, DEFAULT_EMAIL);
+        });
+    }
+
+    private static Stream<String> provideIncorrectPhotos() {
+        return Stream.of(
+                "image-height-oversized.png",
+                "image-length-oversized.png",
+                "oversized.png"
+        );
+    }
+}

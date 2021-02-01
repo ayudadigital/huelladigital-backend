5ad011d92f8c320b647d418a373ab04ebc2e74c6
Juan Manuel <59283605+juanmaMacGyverCode@users.noreply.github.com>
2021-02-01 21:51:46 +0000

    Refactor/rectification swagger (#68)

--- backend/src/main/java/com/huellapositiva/application/dto/ChangeStatusVolunteerDto.java
+++ backend/src/main/java/com/huellapositiva/application/dto/ChangeStatusVolunteerDto.java
@@ -1,5 +1,6 @@
 package com.huellapositiva.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +10,18 @@ import lombok.Data;
 @AllArgsConstructor
 public class ChangeStatusVolunteerDto {
 
+    @Schema(
+            example = "ff79038b-3fec-41f0-bab8-6e0d11db986e"
+    )
     private String idProposal;
+
+    @Schema(
+            example = "ff79038b-3fec-41f0-bab8-6e0d11db986e"
+    )
     private String idVolunteer;
+
+    @Schema(
+            example = "true"
+    )
     private boolean confirmed;
 }

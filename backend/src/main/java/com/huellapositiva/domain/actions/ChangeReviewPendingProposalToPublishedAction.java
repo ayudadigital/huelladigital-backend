0fb01aef863657b4551548c6aea542d4cd9a8212
Manuel <perezregaladomanuel@gmail.com>
2021-02-20 12:05:05 +0000

    * ADD: added action and result DTO

---
+++ backend/src/main/java/com/huellapositiva/domain/actions/ChangeReviewPendingProposalToPublishedAction.java
@@ -0,0 +1,32 @@
+package com.huellapositiva.domain.actions;
+
+import com.huellapositiva.domain.dto.ChangeStatusToPublishedResult;
+import com.huellapositiva.domain.dto.UpdateProfileResult;
+import com.huellapositiva.domain.model.valueobjects.EmailConfirmation;
+import com.huellapositiva.domain.service.EmailCommunicationService;
+import com.huellapositiva.domain.service.ProfileService;
+import com.huellapositiva.domain.service.ProposalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChangeReviewPendingProposalToPublishedAction {
+
+    private final ProposalService proposalService;
+
+    private final EmailCommunicationService emailCommunicationService;
+
+    @Value("${huellapositiva.api.v1.confirmation-email}")
+    private String emailConfirmationBaseUrl;
+
+    public void execute(String idProposal) {
+        ChangeStatusToPublishedResult result = proposalService.changeStatusToPublished(idProposal);
+        /*Implementar mandar email después de la actualización*/
+        /*if (result.isNewEmail()) {
+            EmailConfirmation emailConfirmation = EmailConfirmation.from(result.getEmail(), emailConfirmationBaseUrl);
+            emailCommunicationService.sendMessageEmailChanged(emailConfirmation);
+        }*/
+    }
+}

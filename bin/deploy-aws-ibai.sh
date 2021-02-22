7c88cd4ad83e04e3da6f22a41f3edb76d8fc362f
Ibai Usubiaga <ibai.usubiaga@gmail.com>
2021-02-22 21:24:18 +0000

    Update AWS deploy script

--- bin/deploy-aws-ibai.sh
+++ bin/deploy-aws-ibai.sh
@@ -3,12 +3,11 @@
 env=$1
 AWS_ACCESS_KEY_ID=$2
 AWS_SECRET_ACCESS_KEY=$3
-TASK_DEFINITION=$4
-DOCKER_TAG=$5
+DOCKER_TAG=$4
 
 DOCKER_IMAGE=ayudadigital/huelladigital-backend:${DOCKER_TAG}
-CLUSTER_NAME=dev-ecs-cluster
-TASK_FAMILY=dev-huellapositiva-backend-td
+CLUSTER_NAME="${env}-ecs-cluster"
+TASK_FAMILY="${env}-huellapositiva-backend-td"
 SERVICE_NAME=huellapositiva-backend-ecs-service
 
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "${TASK_FAMILY}" --region "us-east-1")
@@ -16,8 +15,7 @@ echo "======================================"
 echo "Current task definition:"
 echo "${TASK_DEFINITION}"
 echo "======================================"
-# shellcheck disable=SC2086
-NEW_TASK_DEFINITION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$DOCKER_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
+NEW_TASK_DEFINITION=$(echo "$TASK_DEFINITION" | jq --arg IMAGE "$DOCKER_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt) | del(.registeredBy)')
 echo "New task definition:"
 echo "$NEW_TASK_DEFINITION"
 echo "======================================"

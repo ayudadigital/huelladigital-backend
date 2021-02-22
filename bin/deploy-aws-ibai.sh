56a601af044027744668dd1a6d4969ed89e467f7
Ibai Usubiaga <ibai.usubiaga@gmail.com>
2021-02-22 21:11:29 +0000

    Update AWS deploy script

--- bin/deploy-aws-ibai.sh
+++ bin/deploy-aws-ibai.sh
@@ -11,21 +11,26 @@ CLUSTER_NAME=dev-ecs-cluster
 TASK_FAMILY=dev-huellapositiva-backend-td
 SERVICE_NAME=huellapositiva-backend-ecs-service
 
-#TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition ${TASK_DEFINITION_NAME} --region us-east-1)
-#TASK_DEFINITION=$(cat ~/Documents/ayudadigital/backend_task_definition_template.json)
-echo ${TASK_DEFINITION} | jq '.containerDefinitions[0].image='\"${DOCKER_IMAGE}\"
-echo ${TASK_DEFINITION} | jq '.containerDefinitions[0].image='\"${DOCKER_IMAGE}\" > /tmp/new_task_defintion.json
-NEW_TASK_DEFINITION=$(aws ecs register-task-definition --family ${TASK_FAMILY} --region us-east-1 --cli-input-json file:///tmp/new_task_defintion.json)
+TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "us-east-1")
 echo "======================================"
+echo "Current task definition:"
+echo "${TASK_DEFINITION}"
+echo "======================================"
+# shellcheck disable=SC2086
+NEW_TASK_DEFINITION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$DOCKER_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
 echo "New task definition:"
 echo "$NEW_TASK_DEFINITION"
+echo "======================================"
+NEW_TASK_DEFINITION_RESULT=$(aws ecs register-task-definition --region "us-east-1" --cli-input-json "$NEW_TASK_DEFINITION")
+echo "New task definition:"
+echo "${NEW_TASK_DEFINITION_RESULT}"
 echo ""
-NEW_TASK_VERSION=$(echo ${NEW_TASK_DEFINITION} | jq --raw-output '.taskDefinition.revision')
+NEW_TASK_VERSION=$(echo "${NEW_TASK_DEFINITION_RESULT}" | jq --raw-output '.taskDefinition.revision')
 echo "New task version: ${NEW_TASK_VERSION}"
 echo ""
 echo "======================================"
 echo "Updating service with new task definition:"
-NEW_SERVICE_DEFINITION=$(aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_FAMILY:$NEW_TASK_VERSION --region us-east-1)
+NEW_SERVICE_DEFINITION=$(aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_FAMILY:"$NEW_TASK_VERSION" --region us-east-1)
 echo "New service:"
-echo $NEW_SERVICE_DEFINITION
+echo "$NEW_SERVICE_DEFINITION"
 echo ""

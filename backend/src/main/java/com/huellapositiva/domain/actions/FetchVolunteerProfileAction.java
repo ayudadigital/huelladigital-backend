b18103130873af075930aef96a05c89b09fb9f29
Ibai <ibai.usubiaga@gmail.com>
2021-03-19 21:34:51 +0000

    AWS migration (#46)

--- backend/src/main/java/com/huellapositiva/domain/actions/FetchVolunteerProfileAction.java
+++ backend/src/main/java/com/huellapositiva/domain/actions/FetchVolunteerProfileAction.java
@@ -2,6 +2,8 @@ package com.huellapositiva.domain.actions;
 
 import com.huellapositiva.application.dto.GetProfileResponseDto;
 import com.huellapositiva.application.exception.UserNotFoundException;
+import com.huellapositiva.infrastructure.orm.entities.JpaLocation;
+import com.huellapositiva.infrastructure.orm.entities.JpaProfile;
 import com.huellapositiva.infrastructure.orm.entities.JpaVolunteer;
 import com.huellapositiva.infrastructure.orm.repository.JpaVolunteerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,25 +25,29 @@ public class FetchVolunteerProfileAction {
                 .orElseThrow(() -> new UserNotFoundException("Volunteer not found. Account ID: " + accountId));
         GetProfileResponseDto.GetProfileResponseDtoBuilder profileDto = GetProfileResponseDto.builder().email(jpaVolunteer.getCredential().getEmail());
 
-        if (jpaVolunteer.getProfile() != null) {
-            profileDto.name(jpaVolunteer.getProfile().getName())
-                    .surname(jpaVolunteer.getProfile().getSurname())
-                    .birthDate(jpaVolunteer.getProfile().getBirthDate().toString())
-                    .phoneNumber("" + jpaVolunteer.getProfile().getPhoneNumber())
-                    .photo(jpaVolunteer.getProfile().getPhotoUrl())
-                    .curriculumVitae(jpaVolunteer.getProfile().getCurriculumVitaeUrl())
-                    .twitter(jpaVolunteer.getProfile().getTwitter())
-                    .instagram(jpaVolunteer.getProfile().getInstagram())
-                    .linkedin(jpaVolunteer.getProfile().getLinkedin())
-                    .additionalInformation(jpaVolunteer.getProfile().getAdditionalInformation());
+        JpaProfile jpaProfile = jpaVolunteer.getProfile();
+        if (jpaProfile != null) {
+            profileDto.name(jpaProfile.getName())
+                    .surname(jpaProfile.getSurname())
+                    .phoneNumber(jpaProfile.getPhoneNumber())
+                    .photo(jpaProfile.getPhotoUrl())
+                    .curriculumVitae(jpaProfile.getCurriculumVitaeUrl())
+                    .twitter(jpaProfile.getTwitter())
+                    .instagram(jpaProfile.getInstagram())
+                    .linkedin(jpaProfile.getLinkedin())
+                    .additionalInformation(jpaProfile.getAdditionalInformation());
+            if (jpaProfile.getBirthDate() != null) {
+                profileDto.birthDate(jpaProfile.getBirthDate().toString());
+            }
         }
 
-        if (jpaVolunteer.getLocation() != null) {
-            profileDto.province(jpaVolunteer.getLocation().getProvince())
-                    .town(jpaVolunteer.getLocation().getTown())
-                    .address(jpaVolunteer.getLocation().getAddress())
-                    .zipCode(jpaVolunteer.getLocation().getZipCode())
-                    .island(jpaVolunteer.getLocation().getIsland());
+        JpaLocation jpaLocation = jpaVolunteer.getLocation();
+        if (jpaLocation != null) {
+            profileDto.province(jpaLocation.getProvince())
+                    .town(jpaLocation.getTown())
+                    .address(jpaLocation.getAddress())
+                    .zipCode(jpaLocation.getZipCode())
+                    .island(jpaLocation.getIsland());
         }
 
         return profileDto.build();
